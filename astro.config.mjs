b6ac9173142efb117923998aea5c358f7ffc93ee
Ignacio Navas Granizo <navasgranizo.ignacio@gmail.com>
2026-06-14 18:56:36 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import cloudflare from "@astrojs/cloudflare";
 
-// 1. Detectamos si el comando que se está ejecutando es "dev"
-const isDev = process.argv.includes("dev");
-
 export default defineConfig({
   site: "https://revistachelen.com",
-  output: "server", // o "hybrid" dependiendo de tu caso
-
-  integrations: [react(), keystatic(), mdx(), sitemap()],
-
-  // 2. Solo aplicamos el adaptador si NO estamos en local
-  adapter: isDev
-    ? undefined
-    : cloudflare({
-        imageService: "passthrough",
-        // (y cualquier otra configuración de cloudflare que tengas aquí)
-      }),
+  output: "static", // Esto es lo correcto ahora
+  adapter: cloudflare({
+    imageService: "compile",
+    prerenderEnvironment: "node",
+  }),
+  integrations: [mdx(), sitemap(), react(), keystatic()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["@keystatic/astro"],
+    },
+  },
 });
