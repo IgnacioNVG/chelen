74123ea1a68ab7450704f614cbd9a3ea277aebae
Ignacio Navas Granizo <navasgranizo.ignacio@gmail.com>
2026-06-15 13:28:42 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,22 @@ import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 import cloudflare from "@astrojs/cloudflare";
 
+const isCloudflare =
+  process.env.CF_PAGES || process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: "https://revistachelen.com",
-  output: "static", // Esto es lo correcto ahora
-  adapter: cloudflare({
-    imageService: "compile",
-    prerenderEnvironment: "node",
-  }),
+  output: "static",
+
+  // AQUÍ ESTÁ LA CORRECCIÓN:
+  // Si es Cloudflare, usa tu configuración. Si es local, usa undefined.
+  adapter: isCloudflare
+    ? cloudflare({
+        imageService: "compile",
+        prerenderEnvironment: "node",
+      })
+    : undefined,
+
   integrations: [mdx(), sitemap(), react(), keystatic()],
   vite: {
     optimizeDeps: {
